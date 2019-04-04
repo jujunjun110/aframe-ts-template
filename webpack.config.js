dc416ab2c1ba85f0f1390092e89e338f3fd653fb
Jun Ito <jun_ito@voyagegroup.com>
2019-04-04 20:27:10 +0900

    Update: enrich readme

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
         rules: [
             { test: /\.ts$/, use: ["babel-loader", "ts-loader"] },
             { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
-            { test: /\.(glsl|vert|frag)$/, exclude: /node_modules/, use: ['raw-loader', 'glslify-loader'] }]
+            { test: /\.(glsl|vert|frag)$/, exclude: /node_modules/, use: ['raw-loader', 'glslify-loader'] }
+        ]
     },
     resolve: {
         extensions: ['.ts', ".js", "scss", "txt", "frag", "vert"]
